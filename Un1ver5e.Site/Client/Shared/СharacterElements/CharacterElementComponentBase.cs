d684e479b425cd6816b5192be60cbe6c8504b14c
MindHardt <71509337+MindHardt@users.noreply.github.com>
2023-02-11 01:10:16 +0500

    Added basic components

--- Un1ver5e.Site/Client/Shared/СharacterElements/CharacterElementComponentBase.cs
+++ Un1ver5e.Site/Client/Shared/СharacterElements/CharacterElementComponentBase.cs
@@ -5,9 +5,13 @@ namespace Un1ver5e.Site.Client.Shared.СharacterElements;
 
 public class CharacterElementComponentBase : ComponentBase
 {
-    /// <summary>
-    /// The draft being displayed.
-    /// </summary>
-    [Parameter]
-    public CharacterDraft Draft { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+	
+	/// <summary>
+	/// The draft being displayed.
+	/// </summary>
+	[Parameter]
+	public CharacterDraft Draft { get; set; }
+
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 }
